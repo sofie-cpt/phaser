ff5e72533f22735b2676ff9d2a3b7a1f6e730bb4
sofie_chan <sofiechanpt@gmail.com>
2021-12-04 10:27:22 +0800

    making indentaitons

--- app.js
+++ app.js
@@ -14,34 +14,33 @@ function create (){
     game.physics.startSystem(Phaser.Physics.Arcade);
 
 //create the sky 
-game.add.sprite(0,0, 'sky');
-platforms = game.add.physicsGroup();
-platforms.enableBody = true;
+    game.add.sprite(0,0, 'sky');
+    platforms = game.add.physicsGroup();
+    platforms.enableBody = true;
 //create the ground 
-var ground = platform.create(0, 550, 'ground');
-ground.scale.setTo(2.2);
-ground.body.immovable = true;
+    var ground = platform.create(0, 550, 'ground');
+    ground.scale.setTo(2.2);
+    ground.body.immovable = true;
 //create the ledges 
-var ledge = platforms.create(400,400, 'ground');
-ledge.body.immovable = true;
-ledge = platforms.create(-100, 250, 'ground');
-ledge.body.immovable = true;
+    var ledge = platforms.create(400,400, 'ground');
+    ledge.body.immovable = true;
+    ledge = platforms.create(-100, 250, 'ground');
+    ledge.body.immovable = true;
 
-//set text style 
-var style = {font: "bold 32px Arial", fill: "#fff"};
-//positioning the score 
-scorelabel = game.add.text(300,560, "score: ", style);
-scoretext = game.add.text(420, 560, score,style);
-scorelabel.setShadow(3,3,'rgba(0,0,0,0.5',2);
-scoretext.setShadow(3,3,'rgba(0,0,0,0.5',2);
+    //set text style 
+    var style = {font: "bold 32px Arial", fill: "#fff"};
+    //positioning the score 
+    scorelabel = game.add.text(300,560, "score: ", style);
+    scoretext = game.add.text(420, 560, score,style);
+    scorelabel.setShadow(3,3,'rgba(0,0,0,0.5',2);
+    scoretext.setShadow(3,3,'rgba(0,0,0,0.5',2);
 
-//positioning the lives 
-lifelabel = game.add.text(10, 5, "lives: ", style);
-lifetext = game.add.text (120, 5, life,style);
-lifelabel.setShadow(3,3,'rgba(0,0,0,0.5',2);
-lifetext.setShadow(3,3,'rgba(0,0,0,0.5',2);
-
+    //positioning the lives 
+    lifelabel = game.add.text(10, 5, "lives: ", style);
+    lifetext = game.add.text (120, 5, life,style);
+    lifelabel.setShadow(3,3,'rgba(0,0,0,0.5',2);
+    lifetext.setShadow(3,3,'rgba(0,0,0,0.5',2);
+}
 function update (){
 
-}
 }
