5118c23f5ccff784f9cd14675bf372e572913bad
sofie_chan <sofiechanpt@gmail.com>
2021-12-04 10:59:08 +0800

    coded sprite

--- app.js
+++ app.js
@@ -41,6 +41,16 @@ function create(){
 	lifetext = game.add.text(120,5, life,style);
 	lifelabel.setShadow(3,3,'rgba(0,0,0,0.5)',2);
 	lifetext.setShadow(3,3,'rgba(0,0,0,0.5)',2);
+
+    //create player sprite 
+player = game.add.sprite(32, 400, 'dude');
+    //animating the player sprite 
+    player.animations.add('left', [0,1,2,3],10,true);
+    player.animations.add('right', [5,6,7,8],10,true);
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 }
 
 function update(){
