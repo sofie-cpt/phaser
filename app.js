40f9ed7857a5aeed574d02dffc007370d1e64335
sofie_chan <sofiechanpt@gmail.com>
2022-01-15 10:22:12 +0800

    indentations

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function create(){
 	lifetext.setShadow(3,3,'rgba(0,0,0,0.5)',2);
 
     //create player sprite 
-player = game.add.sprite(32, 400, 'dude');
+	player = game.add.sprite(32, 400, 'dude');
     //animating the player sprite 
     player.animations.add('left', [0,1,2,3],10,true);
     player.animations.add('right', [5,6,7,8],10,true);
@@ -51,11 +51,35 @@ player = game.add.sprite(32, 400, 'dude');
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
+
+	// Create the enemy
+	enemy1 = game.add.sprite(760, 20, 'baddie');
+	// Animate the enemy1
+	enemy1.animations.add('left', [0,1], 10, true);
+	enemy1.animations.add('right', [2,3], 10, true);
+	game.physics.arcade.enable(enemy1);
+	enemy1.body.bounce.y = 0.2;
+	enemy1.body.gravity.y = 500;
+	enemy1.body.collideWorldBounds = true;
+	
+	// Create the stars
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	// We will create 12 stars evenly spaced
+	for(var i = 0; i < 12; i++){
+		var star = stars.create(i * 70, 0, 'star');
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+	}
+	
+		// Create keyboard entries
+	cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function update(){
 	game.physics.arcade.collide(player, platforms);
-	game.physics.arcade.collide(star, platforms);
+	game.physics.arcade.collide(stars, platforms);
 	game.physics.arcade.collide(enemy1, platforms);
 
 //reset the players velocity if no events
